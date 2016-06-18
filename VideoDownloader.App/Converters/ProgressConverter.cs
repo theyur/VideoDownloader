0b741fc5a89267cf179bd0f11fd47a7b150ee751
Ruslan <xsabitov@gmail.com>
2016-06-18 23:43:01 +0300

    invalid downloaded files fixed

---
+++ VideoDownloader.App/Converters/ProgressConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace VideoDownloader.App.Converters
+{
+	class ProgressConverter : IValueConverter
+	{
+		#region IValueConverter Members
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			int progress = System.Convert.ToInt32(value);
+			if (progress != -1)
+			{
+				return value;
+			}
+			else
+			{
+				return "undefined size";
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
+}
