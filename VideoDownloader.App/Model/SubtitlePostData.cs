7f9e048e2c92ea2b453b43c0c9ffb580f2a36d80
DESKTOP-P33NE4G\VertexPC <xsabitov@gmail.com>
2018-02-08 20:29:37 +0300

    forgotten files

---
+++ VideoDownloader.App/Model/SubtitlePostData.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoDownloader.App.Model
+{
+    class SubtitlePostData
+    {
+        [JsonProperty("a")]
+        public string Author { get; set; }
+
+        [JsonProperty("cn")]
+        public int ClipIndex { get; set; }
+
+        [JsonProperty("lc")]
+        public string Locale { get; set; }
+       
+        [JsonProperty("m")]
+        public string ModuleName { get; set; }
+    }
+}
---
+++ VideoDownloader.App/Model/ViewclipPostData.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace VideoDownloader.App.Model
+{
+    class ViewclipPostData
+    {
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("clipIndex")]
+        public int ClipIndex { get; set; }
+
+        [JsonProperty("courseName")]
+        public string CourseName { get;set;}
+
+        [JsonProperty("includeCaptions")]
+        public bool IncludeCaptions { get;set;}
+
+        [JsonProperty("locale")]
+        public string Locale { get; set; }
+
+        [JsonProperty("mediaType")]
+        public string MediaType { get; set; }
+
+        [JsonProperty("moduleName")]
+        public string ModuleName { get; set; }
+
+        [JsonProperty("quality")]
+        public string Quality { get; set; }
+
+    }
+}
