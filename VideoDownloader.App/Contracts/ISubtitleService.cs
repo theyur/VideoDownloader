c9ff7045a3107d078f59bb9b91b85bb420f4b8c2
yderevyanko <yderevyanko@ngoar.com>
2018-10-04 14:36:25 +0300

    Subtitles api change fix as of Sep-28-2018

--- VideoDownloader.App/Contracts/ISubtitleService.cs
+++ VideoDownloader.App/Contracts/ISubtitleService.cs
@@ -9,6 +9,6 @@ namespace VideoDownloader.App.Contracts
     interface ISubtitleService
     {
         void Write(string fileName, IList<SrtRecord> subtitleRecords);
-        Task<string> DownloadAsync(HttpHelper httpHelper, string authorId, int partNumber, string moduleName, CancellationToken token);
+        Task<string> DownloadAsync(HttpHelper httpHelper, string authorId, int partNumber, string moduleName, string clipId, CancellationToken token);
     }
 }
